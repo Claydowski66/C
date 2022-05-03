8a43779da81f54e2a98d2354e7ab4bba6f9c6602
Claydowski <clayton.soares.2000@gmail.com>
2022-05-03 15:25:23 -0300

    Exercicio 002

---
+++ exercicios_udemy/secao_03/ex_002.c
@@ -0,0 +1,11 @@
+//Make a program that reads an floating point number and print it.
+
+#include <stdio.h>
+
+int main(){
+    float number;
+
+    printf("Enter a floating point number: ");
+    scanf("%f", &number);
+    printf("The entered number was: %f", number);
+}
