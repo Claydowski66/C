9e5d7c0e1df49f9725a5015d0bb595971e7a8d55
Claydowski <clayton.soares.2000@gmail.com>
2022-05-03 15:00:18 -0300

    Exercicio 001

---
+++ exercicios_udemy/secao_03/ex_001.c
@@ -0,0 +1,12 @@
+//Make a program that reads an integer and print it.
+
+#include <stdio.h>
+
+int main(){
+    int number;
+    
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+    
+    printf("The entered number was: %d", number);
+}
