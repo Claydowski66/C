a6c4c9490c465a9af787384221701156de283ba0
Claydowski <clayton.soares.2000@gmail.com>
2022-05-03 13:48:06 -0300

    Exercicio 001

---
+++ EXERCICIOS_UDEMY/SECAO_3/ex_001.c
@@ -0,0 +1,12 @@
+//Faça um programa que leia um numero inteiro e o imprima.
+
+#include <stdio.h>
+
+int main(){
+    int number;
+    
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &number);
+    
+    printf("O numero digitado foi: %d", number);
+}
