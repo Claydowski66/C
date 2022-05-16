17ddaef417701c83d45655c1b79bfed8f2a486c1
Claydowski <clayton.soares.2000@gmail.com>
2022-05-15 22:19:24 -0300

    Exercicio 048

---
+++ exercicios_udemy/secao_03/ex_048.c
@@ -0,0 +1,17 @@
+//Read an integer value in seconds and print in hours, minutes and seconds.
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int total_seconds, hours, minutes, seconds;
+
+    printf("Enter the total time in seconds: ");
+    scanf("%d", &total_seconds);
+
+    hours = floor(total_seconds / 3600);
+    minutes = floor((total_seconds - (hours * 3600)) / 60);
+    seconds = (total_seconds - (hours * 3600) - (minutes * 60));
+
+    printf("%d seconds is equal to: %d:%d:%02d", total_seconds, hours, minutes, seconds);
+}
