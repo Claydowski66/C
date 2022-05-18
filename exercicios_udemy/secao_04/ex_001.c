6bcdd22c7031630724b41a628b479f22ab80e68e
Claydowski <clayton.soares.2000@gmail.com>
2022-05-18 20:20:35 -0300

    Exercicio 001

---
+++ exercicios_udemy/secao_04/ex_001.c
@@ -0,0 +1,24 @@
+//Write a program that reads two values and prints which one is greater.
+
+#include <stdio.h>
+
+int main()
+{
+    int value1, value2;
+
+    printf("Enter 2 values separated by space: ");
+    scanf("%d %d", &value1, &value2);
+
+    if(value1 > value2)
+    {
+        printf("The value %d is greater than %d.", value1, value2);
+    }
+    else if(value2 > value1)
+    {
+        printf("The value %d is greater than %d.", value2, value1);
+    }
+    else
+    {
+        printf("Both values are the same.");
+    }
+}
