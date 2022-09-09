2664637d92c80595bedfffd04fb22b2947e304e0
Claydowski66 <clayton.soares.2000@gmail.com>
2022-09-09 07:03:56 -0300

    Exercicio 019

--- exercicios_udemy/secao_04/ex_019.c
+++ exercicios_udemy/secao_04/ex_019.c
@@ -1,7 +1,6 @@
 /*Faça um programa para verificar se um determinado número inteiro é divisível por 3 ou 5, mas não simultaneamente pelos dois.*/
 
 #include <stdio.h>
-#include <locale.h>
 
 int main(){
     int escolha;
