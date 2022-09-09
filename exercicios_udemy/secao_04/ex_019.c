8851176184eb490e130c3606a170961b69ce47aa
Claydowski66 <clayton.soares.2000@gmail.com>
2022-09-09 05:33:36 -0300

    Exercicio 019

---
+++ exercicios_udemy/secao_04/ex_019.c
@@ -0,0 +1,30 @@
+/*Faça um programa para verificar se um determinado número inteiro é divisível por 3 ou 5, mas não simultaneamente pelos dois.*/
+
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+    int escolha;
+
+    printf("Digite um número inteiro: ");
+    scanf("%d", &escolha);
+
+    int div_por_3, div_por_5;
+    div_por_3 = escolha % 3;
+    div_por_5 = escolha % 5;
+
+    if(div_por_3 == 0 ^ div_por_5 == 0){
+        if(div_por_3 == 0){
+            printf("O número %d é divisível por 3, mas, não por 5.", escolha);
+        }
+        else{
+            printf("O número %d é divisível por 5, mas, não por 3.", escolha);
+        }
+    }
+    else if(div_por_3 == 0 && div_por_5 == 0){
+        printf("O número %d é divisível tanto por 3 quanto por 5.", escolha);
+    }
+    else{
+        printf("O nnúmero %d não é divisível por 3 e nem por 5.", escolha);
+    }
+}
